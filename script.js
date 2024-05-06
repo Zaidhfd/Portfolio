a3f3d340f83e608f620af57362b273b1f485850f
Zaidhfd <162476147+Zaidhfd@users.noreply.github.com>
2024-05-06 22:28:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// let changeh =()=>{
+//     let about=document.getElementsByClassName('about')[0]
+//     about.style.cssText=`display:none;`
+//     let project=document.getElementsByClassName('projects')[0]
+//     project.style.cssText=`display:none;`
+//     let misc=document.getElementsByClassName('misc')[0]
+//     misc.style.cssText=`display:none;`
+//     let contact=document.getElementsByClassName('contact')[0]
+//     contact.style.cssText=`display:none;`
+// }
+
+
+// let changea =()=>{
+//     let home=document.getElementsByClassName('home')[0]
+//     home.style.cssText=`display:none;`
+//     let project=document.getElementsByClassName('projects')[0]
+//     project.style.cssText=`display:none;`
+//     let misc=document.getElementsByClassName('misc')[0]
+//     misc.style.cssText=`display:none;`
+//     let contact=document.getElementsByClassName('contact')[0]
+//     contact.style.cssText=`display:none;`
+//     document.addEventListener('click',(e)=>{
+//     if(e.target.nodeName==='BODY'){
+//         search.classlist.remove('active')
+//     }    
+//     })
+// }
